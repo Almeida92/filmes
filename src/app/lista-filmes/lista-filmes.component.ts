19cec18bc34b82e118b358d4729e4d04b38041e0
Felipe almeida <felipepereira937@gmail.com>
2018-04-18 19:38:36 -0300

    input

--- src/app/lista-filmes/lista-filmes.component.ts
+++ src/app/lista-filmes/lista-filmes.component.ts
@@ -1,3 +1,4 @@
+import { Filme } from './../filme';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-filmes.component.css']
 })
 export class ListaFilmesComponent implements OnInit {
+
+  filmes: Filme[] = [
+    {
+      "titulo": "Batman Begins",
+      "ano": "2005",
+      "id": "tt0372784",
+      "tipo": "movie",
+      "poster": "https://ia.media-imdb.com/images/M/MV5BYzc4ODgyZmYtMGFkZC00NGQyLWJiMDItMmFmNjJiZjcxYzVmXkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg"
+    },
+    {
+      "titulo": "Batman v Superman: Dawn of Justice",
+      "ano": "2016",
+      "id": "tt2975590",
+      "tipo": "movie",
+      "poster": "https://ia.media-imdb.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+    },
+    {
+      "titulo": "Batman",
+      "ano": "1989",
+      "id": "tt0096895",
+      "tipo": "movie",
+      "poster": "https://ia.media-imdb.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg"
+    }
+  ];
 
   constructor() { }
 
